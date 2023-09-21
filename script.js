709e169f501902a026bb49d3a19a27f3ea42706e
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-21 14:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 //your code here
+// function get(){
+// 	const list=document.getElementById("todoList");
+// 	const li=document.createElement("li");
+// 	li.innerHTML=document.getElementById("newTodoInput").value;
+// 	list.appendChild(li);
+// }
+// const button=document.getElementById("addTodoBtn");
+// button.addEventListener("click",get);
 function get(){
-	const list=document.getElementById("todoList");
-	const li=document.createElement("li");
-	li.innerHTML=document.getElementById("newTodoInput").value;
-	list.appendChild(li);
+    const list = document.getElementById("todoList");
+    const li = document.createElement("li");
+    li.innerHTML = document.getElementById("newTodoInput").value;
+    list.appendChild(li);
+
+    // Clear the input box
+    document.getElementById("newTodoInput").value = "";
 }
-const button=document.getElementById("addTodoBtn");
-button.addEventListener("click",get);
+
+const button = document.getElementById("addTodoBtn");
+button.addEventListener("click", get);
