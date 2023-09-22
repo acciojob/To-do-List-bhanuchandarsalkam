a0ad37749705a77a288611432b087e62853f3e9d
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-22 14:36:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,9 +10,14 @@
 function get(){
     const list = document.getElementById("todoList");
     const li = document.createElement("li");
-    li.innerHTML = document.getElementById("newTodoInput").value;
+	const val=document.getElementById("newTodoInput").value;
+	if(!val){
+		get();
+	}
+	else{
+		li.innerHTML = document.getElementById("newTodoInput").value;
     list.appendChild(li);
-
+	}
     // Clear the input box
     document.getElementById("newTodoInput").value = "";
 }
